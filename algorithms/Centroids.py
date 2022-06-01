f36b3b5d1fe3166189d2759630769cd08909ee9a
anison99_ <anita.zych9941@gmail.com>
2022-06-01 17:34:30 +0200

    add  skeletinization code

--- algorithms/Centroids.py
+++ algorithms/Centroids.py
@@ -8,7 +8,7 @@ from main import image
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # convert the grayscale image to binary image
-ret,thresh = cv2.threshold(gray_image,127,255,0)
+ret, thresh = cv2.threshold(gray_image, 127, 255, 0)
 
 # calculate moments of binary image
 M = cv2.moments(thresh)
@@ -19,7 +19,7 @@ cY = int(M["m01"] / M["m00"])
 
 # put text and highlight the center
 cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
-cv2.putText(image, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+cv2.putText(image, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
 # display the image
 cv2.imshow("Image", image)
